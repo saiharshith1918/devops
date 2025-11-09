594a7ccf99f678988fee7099c68e53f053154974
Sai Harshith Reddy <saiharshithreddy383@gmail.com>
2025-11-09 08:19:38 +0530

    testing jenkins auto-build

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("🔥 Deployed via Jenkins + Docker + Kubernetes 🔥");
+  res.send("🚀 Automated test number 2!");
+
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
