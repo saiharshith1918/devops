e6dc87cc072a57bb5ad49293bfbeff80e0cee181
Sai Harshith Reddy <saiharshithreddy383@gmail.com>
2025-11-09 12:03:20 +0530

    fixed express response and bind to 0.0.0.0

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
   console.log("Through poll scm, testing at 11:34");
+  res.send("✅ Jenkins pipeline test successful — app is running!");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
