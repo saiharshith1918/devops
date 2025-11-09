b2a2d1b0c1e201ef25b33fc761f92965e003b6f4
Sai Harshith Reddy <saiharshithreddy383@gmail.com>
2025-11-09 10:03:58 +0530

    poll scm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  console.log("Hello from Jenkins Auto Build Test v2!");
+  console.log("Through poll scm");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
